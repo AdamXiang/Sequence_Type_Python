e5fa169a70262fc1ba062805e7b778c52558c916
Adam Chang <50445460+AdamXiang@users.noreply.github.com>
2025-08-22 10:52:08 +0800

    create the polygon class

---
+++ custom_sequence/polygon.py
@@ -0,0 +1,48 @@
+import math
+
+
+class Polygon:
+    def __init__(self, vertices_number, circumradius):
+        self._vertices_number = vertices_number
+        self._circumradius = circumradius
+
+    def __repr__(self):
+        return f"Polygon(vertices_number={self._vertices_number}, circumradius={self._circumradius})"
+
+    @property
+    def count_vertice(self):
+        return self._vertices_number
+
+    @property
+    def count_edge(self):
+        return self._vertices_number
+
+    @property
+    def circumradius(self):
+        return self._circumradius
+
+    @property
+    def interior_angle(self):
+        return (self._n - 2) * 180 / n 
+
+    @property
+    def side_length(self):
+        return 2 * self._circumradius * math.sin(math.pi / self._vertices_number)
+
+    @property
+    def apothem(self):
+        return self._circumradius * math.cos(math.pi / self._vertices_number)
+
+    @property
+    def area(self):
+        return self._vertices_number / 2 * self.side_length * self.apothem
+
+    @property
+    def perimeter(self):
+        return self._vertices_number * self.side_length
+
+
+# make the test
+def test_polygon():
+    
+
